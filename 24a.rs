859ced52f33d031a6334092002f29c38921b2995
never <nevernevernever69@protonmail.com>
2023-01-29 20:17:31 +0530

    some rust exercise

--- 24a.rs
+++
@@ -1,9 +0,0 @@
-fn main(){
-
-    let mut data = vec![1,2,3,4,5];
-    let new_arr: Vec<_> = data.iter().map(|num| num * 3).filter(|num| num > &10).collect();
-    for i in new_arr{
-        println!("{}", i);
-    }
-
-}
